18c3e3ace72f29cfb61f05bb36d79f1c17ef1252
michalpawlyna <michal.pawlyna@wp.pl>
2026-06-30 15:13:05 +0200

    fixed vercel analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-import { inject } from '@vercel/analytics'
+import { Analytics } from "@vercel/analytics/vue"
 
 createApp(App).mount('#app')
 
